10427a39c47ddc0ac7a5187ed2e525f2e9ce367e
Shoukathsandy <shoukathsandy@gmail.com>
2022-10-22 16:30:36 +0530

    password

--- src/globel.js
+++ src/globel.js
@@ -1,3 +1,3 @@
 // https://qutation-maker.herokuapp.com/
-// export const API = "https://qutation-maker.herokuapp.com";
-export const API ="http://localhost:5000"
+export const API = "https://qutation-maker.herokuapp.com";
+// export const API ="http://localhost:5000"
