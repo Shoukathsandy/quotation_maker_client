cd2bd5ef76e8d2b6cb0b80f825f526e5b6ece304
Shoukathsandy <shoukathsandy@gmail.com>
2023-02-03 12:42:28 +0530

    render.1

--- src/globel.js
+++ src/globel.js
@@ -1,3 +1,4 @@
 // https://qutation-maker.herokuapp.com/
-export const API = "https://qutation-maker.herokuapp.com";
+// export const API = "https://qutation-maker.herokuapp.com";
+export const API = "https://quotaion-maker.onrender.com";
 // export const API ="http://localhost:5000"
